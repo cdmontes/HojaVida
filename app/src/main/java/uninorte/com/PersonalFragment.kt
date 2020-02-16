e445e4bd6986a831d73491e01155c46fd1204ee0
Carlos <cdmontes@uninorte.edu.co>
2020-02-16 18:13:26 -0500

    Commit inicial Hoja de Vida con Navigation Components

---
+++ app/src/main/java/uninorte/com/PersonalFragment.kt
@@ -0,0 +1,32 @@
+package uninorte.com
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import uninorte.com.model.UserPersonalModel
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PersonalFragment : Fragment() {
+
+    lateinit var user: UserPersonalModel
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_personal, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        user = arguments!!.getParcelable("data")!!
+        view.findViewById<TextView>(R.id.textViewHobby).text = user.hobby
+        view.findViewById<TextView>(R.id.textViewNombre).text = arguments!!.getString("nombre")!!
+    }
+}
---
+++ app/src/main/java/uninorte/com/model/UserPersonalModel.kt
@@ -0,0 +1,14 @@
+package uninorte.com.model
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+class UserPersonalModel (
+    val name: String,
+    val edad: Int,
+    val hobby: String,
+    val numeroHermanos: Int ) : Parcelable {
+}
